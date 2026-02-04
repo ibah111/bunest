ba4a15e401d9854935f3ad32934c70d8056b258f
ibah <starlord77@mail.ru>
2026-02-04 12:38:58 +0300

    refactor: removed

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppPageModule } from './healthcheck/healthcheck.module';
-import { WsaModule } from './wsa/wsa.module';
 import { TipsModule } from './tips/tips.module';
 
 @Module({
-  imports: [AppPageModule, WsaModule, TipsModule],
+  imports: [AppPageModule, TipsModule],
 })
 export class PagesModule { }
