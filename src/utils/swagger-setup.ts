fd30a203a68a630f1c3507915c8b25ab1fc0145c
ibah <starlord77@mail.ru>
2026-06-02 10:35:22 +0300

    fix: little changes to swagger logs

--- src/utils/swagger-setup.ts
+++ src/utils/swagger-setup.ts
@@ -1,14 +1,15 @@
 import { DOCS_PATH } from '@/consts/application';
-import type { INestApplication } from '@nestjs/common';
+import { Logger, type INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import type { Request, Response, NextFunction } from 'express';
 
-
 export function swaggerSetup(app: INestApplication): void {
+  const logger = new Logger(swaggerSetup.name.toUpperCase());
   app.use((req: Request, _res: Response, next: NextFunction) => {
     const path = req.path ?? req.originalUrl ?? '';
     if (path.startsWith(DOCS_PATH)) {
       const ua = req.get('User-Agent') ?? req.headers['user-agent'] ?? 'unknown';
+      logger.verbose("Swagger logs:")
       console.log(`${req.method} ${path} | User-Agent: ${ua}`);
     }
     next();
